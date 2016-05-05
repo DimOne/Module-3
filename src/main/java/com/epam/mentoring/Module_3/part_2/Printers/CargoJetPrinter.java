dbb9aa0f6dabeecfa6f23dac072d86e7f6a3abad
Dmytro Ovcharenko <dmytro_ovcharenko1@epam.com>
2016-05-05 17:53:26 +0300

    changes from 05/05.

--- src/main/java/com/epam/mentoring/Module_3/part_2/Printers/CargoJetPrinter.java
+++ src/main/java/com/epam/mentoring/Module_3/part_2/Printers/CargoJetPrinter.java
@@ -9,22 +9,26 @@ public class CargoJetPrinter implements Printer {
 
     @Override
     public void printType(String type) {
-       System.out.print(type);
+        System.out.print(type);
     }
 
     @Override
     public void printModel(String model) {
-       System.out.print(model);
+        System.out.print(model);
+    }
+
+    public void printCarryingCapacity(long carrying_capacity) {
+        System.out.print(carrying_capacity);
     }
 
     @Override
     public void printWeight(int weight) {
-       System.out.print(weight);
+        System.out.print(weight);
     }
 
     @Override
     public void printDistance(double distance) {
-         System.out.print(distance);
+        System.out.print(distance);
     }
 
 }
