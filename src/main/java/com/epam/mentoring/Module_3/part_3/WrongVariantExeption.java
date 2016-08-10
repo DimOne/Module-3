e699fc973c65506049d1486cc849bba7e592fdac
Dmytro Ovcharenko <dmytro_ovcharenko1@epam.com>
2016-08-10 17:43:56 +0300

    changes from 10/08.

---
+++ src/main/java/com/epam/mentoring/Module_3/part_3/WrongVariantExeption.java
@@ -0,0 +1,9 @@
+package com.epam.mentoring.Module_3.part_3;
+
+public class WrongVariantExeption extends Exception {
+
+    public WrongVariantExeption() {
+        super("You entered invalid value!");
+    }
+
+}
